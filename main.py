9f2d33090101739c962f48ac2ae50f7a3be7a2cb
Trace Pulse <138300084+Piratezis@users.noreply.github.com>
2025-07-23 21:54:58 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,1 @@
-import asyncio
-from telethon.sync import TelegramClient
-from telethon.tl.functions.messages import GetHistoryRequest
-from telethon.tl.types import MessageReactions, MessageReaction
+import sys
